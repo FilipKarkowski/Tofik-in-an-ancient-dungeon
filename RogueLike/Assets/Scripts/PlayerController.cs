524375d500b19646c07d665c9469e64b84642090
Filip Karkowski <114432611+Xeoaxis@users.noreply.github.com>
2024-03-02 19:19:26 +0100

    Player
    Added player base model and animation

--- RogueLike/Assets/Scripts/PlayerController.cs
+++ RogueLike/Assets/Scripts/PlayerController.cs
@@ -9,26 +9,49 @@ public class PlayerController : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
+
     private void Awake(){
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
     private void Update() {
         PlayerInput();
     }
     private void FixedUpdate() {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
 
     private void PlayerInput()
     {
+        
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
-       
+        myAnimator.SetFloat("moveX",movement.x);
+        myAnimator.SetFloat("moveY",movement.y);
         Debug.Log(movement.x);
     }    
+
     private void Move(){
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection(){
+        Vector3 mousepos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        if(mousepos.x < playerScreenPoint.x){
+           mySpriteRenderer.flipX = false;
+            Debug.Log("nah");
+        }
+        else{
+           
+              mySpriteRenderer.flipX = true;
+            Debug.Log("flipped");
+        }
     }
 
     private void OnEnable() {
