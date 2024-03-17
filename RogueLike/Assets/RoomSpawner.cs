d7c6fb6308aa7465c952566068ce9432eee12e53
Filip Karkowski <114432611+Xeoaxis@users.noreply.github.com>
2024-03-17 12:39:43 +0100

    Room Error Fix
    Fixed null exception with  room closing

--- RogueLike/Assets/RoomSpawner.cs
+++ RogueLike/Assets/RoomSpawner.cs
@@ -25,21 +25,25 @@ public class RoomSpawner : MonoBehaviour
 		case 1:
 		Rand = Random.Range(0,roomTemplates.bottomRooms.Length);
 		Instantiate(roomTemplates.bottomRooms[Rand],transform.position,roomTemplates.bottomRooms[Rand].transform.rotation);
+		Debug.Log($"room: {this.transform.parent.name}, spawned room in {this.name} spawnpoint ");
 		break;
 		case 2:
 		// need to spawn rom with top door
 		Rand = Random.Range(0,roomTemplates.topRooms.Length);
 		Instantiate(roomTemplates.topRooms[Rand],transform.position,roomTemplates.topRooms[Rand].transform.rotation);
+		Debug.Log($"room: {this.transform.parent.name}, spawned room in {this.name} spawnpoint ");
 		break;
 		case 3:
 		// need to spawn rom with left door
 		Rand = Random.Range(0,roomTemplates.leftRooms.Length);
 		Instantiate(roomTemplates.leftRooms[Rand],transform.position,roomTemplates.leftRooms[Rand].transform.rotation);
+		Debug.Log($"room: {this.transform.parent.name}, spawned room in {this.name} spawnpoint ");
 		break;
 		case 4:
 		// need to spawn rom with right door
 		Rand = Random.Range(0,roomTemplates.rightRooms.Length);
 		Instantiate(roomTemplates.rightRooms[Rand],transform.position,roomTemplates.rightRooms[Rand].transform.rotation);
+		Debug.Log($"room: {this.transform.parent.name}, spawned room in {this.name} spawnpoint ");
 		break;
 
 	}
@@ -51,7 +55,9 @@ public class RoomSpawner : MonoBehaviour
 	{
 		if(other.GetComponent<RoomSpawner>().spawned == false && spawned == false )
 		{
+			roomTemplates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
 			Instantiate(roomTemplates.closedRooms,transform.position, Quaternion.identity);
+			Debug.Log($"Closed room created");
 			Destroy(gameObject);
 		}
 		spawned = true;
