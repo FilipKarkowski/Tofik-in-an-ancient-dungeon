1758b2cf713458ca41d97472b6d51fd3210da689
Filip Karkowski <114432611+Xeoaxis@users.noreply.github.com>
2024-03-17 11:00:07 +0100

    RoomGenerator with boss spawn

--- RogueLike/Assets/RoomTemplates.cs
+++ RogueLike/Assets/RoomTemplates.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class RoomTemplates : MonoBehaviour
@@ -12,4 +13,31 @@ public class RoomTemplates : MonoBehaviour
 	public GameObject closedRooms;
 	
 	public List<GameObject> rooms;
+	
+	public float waitTime;
+	private bool spawnedBoss = false;
+	
+	public GameObject boss;
+	
+	private void Update() {
+		if(waitTime <= 0 && spawnedBoss == false)
+		{
+			for(int i = 0; i < rooms.Count; i++)
+			{
+				if(i == rooms.Count-1)
+				{
+					Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+					spawnedBoss = true;
+					Debug.Log("Spawned boss");
+				}
+				
+				Debug.Log(rooms.Count);
+			}
+		}
+		else
+		{
+			waitTime -= Time.deltaTime;
+		}
+	}
+	
 }
