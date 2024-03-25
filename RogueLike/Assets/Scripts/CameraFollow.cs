a2ab167e0a8548b24e01deb45847e990c299c340
Filip Karkowski <114432611+Xeoaxis@users.noreply.github.com>
2024-03-25 21:21:20 +0100

    Camera follow and room  upgrade

---
+++ RogueLike/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+ public Transform target;
+ public float smoothing;
+ public Vector2 maxPos;
+ public Vector2 minPos;
+ private void Start() {
+   target = FindObjectOfType<PlayerController>().transform;
+ }
+ private void FixedUpdate() {
+    if(transform.position !=target.position){
+        Vector3 targetpos = new Vector3(target.position.x,target.position.y, transform.position.z);
+        targetpos.x =Mathf.Clamp(targetpos.x,minPos.x,maxPos.x);
+         targetpos.y =Mathf.Clamp(targetpos.y,minPos.y,maxPos.y);
+         transform.position = Vector3.Lerp(transform.position,targetpos,smoothing);
+    }
+ }
+}
