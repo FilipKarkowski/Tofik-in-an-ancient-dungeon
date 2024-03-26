27a8e52fed39989c46b72e137d7309d00074b8db
Filip Karkowski <114432611+Xeoaxis@users.noreply.github.com>
2024-03-26 09:50:06 +0100

    LayersPosition and camera shake
    FIxed layers positions and added camera shake script

--- RogueLike/Assets/Scripts/EnemyAttack.cs
+++ RogueLike/Assets/Scripts/EnemyAttack.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
 
-    //simple enemy attack 
-    [SerializeField] private int damagAmount = 1;
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.gameObject.GetComponent<PlayerStats>()){ // if gameobject is player take some damage amount from him 
-            PlayerStats playerStats = other.gameObject.GetComponent<PlayerStats>();
-            playerStats.TakeDamage(damagAmount); 
-        }
-    }
+	//simple enemy attack 
+	[SerializeField] private int damagAmount = 1;
+	private void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.GetComponent<PlayerStats>()){ // if gameobject is player take some damage amount from him 
+			CameraShake playercam = FindAnyObjectByType<CameraShake>();
+			PlayerStats playerStats = other.gameObject.GetComponent<PlayerStats>();
+			playerStats.TakeDamage(damagAmount);
+			
+		}
+	}
 }
+
+
